f3581358b78867953f25cd2386cbcb4378b7ec80
showcomm <lowell.cochrane@gmail.com>
2026-01-26 21:09:27 -0500

    Remove React StrictMode - fixes gaussian-splats-3d initialization issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
 
+// Note: StrictMode removed because gaussian-splats-3d library
+// doesn't handle React's double-mount/unmount cycle properly.
+// StrictMode is only active in development anyway.
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
